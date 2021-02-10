df9ba0b8c47039de8fd1000d9c1ab319fa48d4b7
venky9876 <venkatesh.gummadi@gmail.com>
2021-02-10 10:06:33 +0530

    multiply def

---
+++ programming/multiply.py
@@ -0,0 +1,21 @@
+# Multiply two non negative integers using only bitwise operators
+
+def bit_multiply(x,y):
+    product = 0
+
+    
+
+    return product
+
+
+if __name__ == "__main__":
+
+    x = 10
+    y = 20
+
+    prod = bit_multiply(x,y)
+
+    print ("product of %s and %s is %s"%(x,y,prod))
+
+
+
